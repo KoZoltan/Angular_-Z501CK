10532007753def160e5600e4a06bae0243cc6a19
KoZoltan <kocsis.zoltan.2001.06.07@gmail.com>
2024-05-04 23:09:56 +0200

    tutorial vegigtolva

--- src/app/page/patient-editor/patient-editor.component.ts
+++ src/app/page/patient-editor/patient-editor.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, Input, inject, numberAttribute } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { PatientService } from '../../service/patient.service';
+import { Observable, of } from 'rxjs';
+import { Patient } from '../../model/patient';
 
 @Component({
   selector: 'app-patient-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, AsyncPipe, FormsModule],
   templateUrl: './patient-editor.component.html',
   styleUrl: './patient-editor.component.css'
 })
 export class PatientEditorComponent {
 
-}
+  @Input({required: true, transform: numberAttribute}) TAJ: number = 0;
+
+  patientService = inject(PatientService);
+  router = inject(Router);
+
+  patient$: Observable<Patient> = of(new Patient());
+
+  ngOnInit(): void {
+    if (this.TAJ) {
+      this.patient$ = this.patientService.get(this.TAJ);
+      }
+    }
+
+
+    onUpdate(patient: Patient): void {
+      if(this.TAJ) {
+        this.patientService.update(patient).subscribe(() => {
+          this.router.navigate(['/patient']);
+        });
+      }
+  
+    }
+
+
+    
+
+
+
+
+
+
+  }
+
+
+
